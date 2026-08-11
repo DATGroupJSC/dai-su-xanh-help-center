83b0f1b93a1224f8b4eee9c74d26f145b78c28aa
CodexSandboxOffline <CodexSandboxOffline@KYTHUATDAT437.corp.datgroup.com.vn>
2026-08-11 16:39:10 +0700

    chore: bootstrap dai su xanh help center

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Layout from '@theme/Layout';
+
+export default function BootstrapHomePage() {
+  return (
+    <Layout title="Help Center bootstrap">
+      <main className="container margin-vert--xl">
+        <h1>Help Center bootstrap</h1>
+      </main>
+    </Layout>
+  );
+}
